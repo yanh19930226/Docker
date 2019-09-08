3629459e470c45bdc59881c9c4ffe441655b4434
891367701@qq.com <891367701@qq.com>
2019-09-08 13:05:41 +0800

    'up'

---
+++ console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Thread.Sleep(Timeout.Infinite);
+            // Console.ReadLine();
+        }
+    }
+}
